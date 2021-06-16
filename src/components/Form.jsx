8ab71a117529fc19bc5dde73d38ad2a1eb276d03
MouadCHAAIRA <mouad.chaaira@usmba.ac.ma>
2021-06-16 09:46:35 +0100

    this morning

---
+++ src/components/Form.jsx
@@ -0,0 +1,100 @@
+import React, { useContext, useState } from 'react'
+import { GalleryContext } from '../context'
+import { motion } from 'framer-motion'
+
+const Form = () => {
+  const { uploadFile, error, data, progress, deleteGallerie } =
+    useContext(GalleryContext)
+  const [file, setFile] = useState(null)
+  const [selectedImage, setSelectedImage] = useState('')
+
+  document.body.style.backgroundSize = 'cover'
+  document.body.style.backgroundPosition = 'center'
+  document.body.style.backgroundRepeat = 'no-repeat'
+  document.body.style.background = `url('${selectedImage}')`
+
+  const changeHandle = (e) => {
+    const _selectedFile = e.target.files[0]
+    if (_selectedFile) {
+      setFile(_selectedFile)
+      uploadFile(_selectedFile)
+    } else {
+      setFile(null)
+    }
+  }
+
+  return (
+    <>
+      <p>{error && error}</p>
+      <form className='mb-5'>
+        <label htmlFor='image'>Image</label>
+        <input
+          className='form-control'
+          multiple={false}
+          id='image'
+          type='file'
+          onChange={changeHandle}
+        />
+      </form>
+
+      <div className='progress'>
+        <div
+          className='progress-bar'
+          role='progressbar'
+          style={{ width: progress + '%' }}
+          aria-valuemin='0'
+          aria-valuemax='100'
+        >
+          {progress.toFixed(1) + '%'}
+        </div>
+      </div>
+
+      <div className='album py-5 '>
+        <div className='container'>
+          <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+            {data &&
+              data.map(({ docId, url }) => {
+                // import { motion } from 'framer-motion'
+                return (
+                  <div className='picture'>
+                    <span
+                      onClick={() => {
+                        deleteGallerie(docId)
+                        setSelectedImage(' ')
+                      }}
+                      className='clear-img'
+                    >
+                      ❌
+                    </span>
+                    <motion.div
+                      onClick={() => {
+                        setSelectedImage(url)
+                      }}
+                      style={{ cursor: 'pointer' }}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ delay: 1.2 }}
+                      layout
+                      key={docId}
+                      width='100'
+                      className='col shadow-sm'
+                    >
+                      <img
+                        height='100%'
+                        width='100%'
+                        className='bd-placeholder-img card-img-top'
+                        src={url}
+                        alt='test'
+                      />
+                    </motion.div>
+                  </div>
+                )
+              })}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Form
--- src/context.js
+++ src/context.js
@@ -4,7 +4,76 @@ import { db, storage, timestamp } from './firebase/config'
 const GalleryContext = createContext()
 
 const GalleryProvider = ({ children }) => {
-  const values = {}
+  const [progress, setProgress] = useState(0)
+  const [error, setError] = useState('')
+  const [url, setUrl] = useState('')
+  const [file, setFile] = useState(null)
+  const [data, setData] = useState([])
+
+  const uploadFile = (_file) => {
+    setFile(_file)
+  }
+
+  const getGallery = () => {
+    db.collection('gallery')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snapshot) => {
+        const _data = snapshot.docs.map((doc) => {
+          return { docId: doc.id, ...doc.data() }
+        })
+        setData(_data)
+      })
+  }
+
+  useEffect(() => {
+    getGallery()
+  }, [])
+
+  useEffect(() => {
+    if (file) {
+      const stgRef = storage.ref(file.name)
+      const collectionRef = db.collection('gallery')
+
+      stgRef.put(file).on(
+        'state_changed',
+        (snap) => {
+          // uploading
+          const percentage = (snap.bytesTransferred / snap.totalBytes) * 100
+          setProgress(percentage)
+          console.log(percentage)
+        },
+        (err) => {
+          // on error
+          setError(err)
+        },
+        () => {
+          // on finish upload
+          stgRef.getDownloadURL().then((url) => {
+            setUrl(url)
+            collectionRef.add({
+              createdAt: timestamp(),
+              url,
+            })
+            setProgress(0)
+            setFile(null)
+          })
+        }
+      )
+    }
+  }, [file])
+
+  const deleteGallerie = (docId) => {
+    db.collection('gallery').doc(docId).delete()
+  }
+
+  const values = {
+    progress,
+    url,
+    error,
+    uploadFile,
+    data,
+    deleteGallerie,
+  }
   return <GalleryContext.Provider value={values} children={children} />
 }
 
